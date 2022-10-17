452e6f85bbd908f38fd9a88143488d628e806259
AKhil Kukadiya <akhilkukadiya464@gmail.com>
2022-10-17 22:43:09 +0530

    17-10 error handling
    res messages and error handling for product controller

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,4 +1,5 @@
-const Product = require('../models/productModel')
+const Product = require('../models/productModel');
+const ErrorHandlor = require('../utils/errorHandler');
 
 //create Product -- Admin
 exports.createProduct= async(req, res, next) =>{
@@ -27,10 +28,7 @@ exports.updateProduct = async(req, res, next)=>{
     let product = await Product.findById(req.params.id);
 
     if(!product){
-        return res.status(500).json({
-            success:false,
-            message:"product not found"
-        })
+        return next(new ErrorHandlor("product not found", 404))
     }
     product = await Product.findByIdAndUpdate(req.params.id, req.body, {new:true, runValidators:true, useFindAndModify:false});
 
@@ -46,10 +44,8 @@ exports.deleteProduct = async(req, res, next)=>{
     let product = await Product.findById(req.params.id);
 
     if(!product){
-        return res.status(500).json({
-            success:false,
-            message:"product not found"
-        })
+        return next(new ErrorHandlor("product not found", 404))
+        
     }
 
     await product.remove();
@@ -65,10 +61,7 @@ exports.getProduct= async(req, res, next)=>{
     let product = await Product.findById(req.params.id);
 
     if(!product){
-        return res.status(500).json({
-            success:false,
-            message:"product not found"
-        })
+        return next(new ErrorHandlor("product not found", 404))
     }
     res.status(200).json({
         success:true,
