346ad903aad4d926cfd240c37771f2b73b42aed6
AKhil Kukadiya <akhilkukadiya464@gmail.com>
2022-10-18 08:25:05 +0530

    1.2

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,10 @@ const dotenv = require('dotenv')
 const connectDB = require('./config/database')
 
 //consfig
-dotenv.config({path:"backend/config/config.env"})
+dotenv.config({path:"backend/config/.env"})
 
 connectDB()
 
 app.listen(process.env.PORT , ()=>{
     console.log(`server is running on http://localhost:${process.env.PORT}`)
-})
+}) 
